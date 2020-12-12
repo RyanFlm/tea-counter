1440beed5b1b996084f98798162aa9d8f152fb09
RyanFlm <info@apelma.de>
2020-12-12 23:18:45 +0100

    Small tea counter app implemented

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import './App.scss';
+
+function App() {
+
+    const initial = parseInt(localStorage.getItem('counter')) || 0;
+    const [ count, setCount ] = useState(initial);
+
+    useEffect(() => localStorage.setItem('counter', count.toString()));
+
+    return (
+        <div className={'container'}>
+            <div className={'actionArea'}>
+                <h1 className={'title'}>Drunken Cups of Tea:</h1>
+                <button className={'count'} type={'button'} onClick={() => setCount(0)}>
+                    {count}
+                </button>
+                <button className={'countUp'} type={'button'} onClick={() => setCount(count + 1)}>
+                    +
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
